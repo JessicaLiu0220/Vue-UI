196d2d6986508aa87d7a1a8206fbd99119d68c84
Jessica <1330784336@qq.com>
2022-03-17 20:21:59 +0800

    优化Switch的css样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -25,24 +25,27 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: grey;
+  background: #ff4949;
   border-radius: $h/2;
   position: relative;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 250ms;
+  }
+  &.checked {
+    background: #13ce66;
+  }
+  &.checked > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
 }
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
-}
-button.checked {
-  background: blue;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
+button:focus {
+  outline: none;
 }
 </style>
