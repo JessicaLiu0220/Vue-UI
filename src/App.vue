15a2e1a2a8de9031143ed69dd8aa4da52b4574c4
Jessica <1330784336@qq.com>
2022-03-17 12:18:23 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hi</div>
+  <liu />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Liu from "./components/liu.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Liu,
+  },
+};
+</script>
