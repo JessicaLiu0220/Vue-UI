1aa504911648a78ebecacb9d59a31dc3cc709915
Jessica <1330784336@qq.com>
2022-03-17 17:53:34 +0800

    点击组件显示相应的文档

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -27,7 +27,10 @@ export default {
   background: pink;
   display: flex;
   padding: 16px;
-  position: relative;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
   z-index: 10;
   justify-content: center;
   align-items: center;
