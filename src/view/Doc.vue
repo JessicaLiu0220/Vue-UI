6df4433fbb02b3d8faea45cf343baa3a2da15b27
Jessica <1330784336@qq.com>
2022-03-17 14:07:05 +0800

    完成aside

--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -2,7 +2,15 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li><router-link to="/doc/switch">switch</router-link></li>
+          <li><router-link to="/doc/button">button</router-link></li>
+          <li><router-link to="/doc/dialog">dialog</router-link></li>
+          <li><router-link to="/doc/tabs">tabs</router-link></li>
+        </ol>
+      </aside>
       <main>主内容111</main>
     </div>
   </div>
@@ -13,3 +21,22 @@ export default {
   components: { Topnav },
 };
 </script>
+<style lang="scss" scope>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  padding-top: 70px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
