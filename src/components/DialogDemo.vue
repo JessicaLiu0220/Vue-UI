dd3ef025438b1a419a4159645890bec78c5cd611
Jessica <1330784336@qq.com>
2022-03-18 15:26:55 +0800

    Dialog支持点击

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>dialog</div>
+  <div>Dialog示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x" :ok="f1" :cancel="f2" />
 </template>
+
+<script>
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return true;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  },
+};
+</script>
