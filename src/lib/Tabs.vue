289c8a2841120d0484b0ffcbac633f5272d628e2
Jessica <1330784336@qq.com>
2022-03-19 16:28:53 +0800

    让下划线宽度等于title宽度

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,15 +1,22 @@
+
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
       <div
         class="gulu-tabs-nav-item"
         v-for="(t, index) in titles"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
         @click="select(t)"
         :class="{ selected: t === selected }"
         :key="index"
       >
         {{ t }}
       </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component
@@ -25,25 +32,35 @@
 
 <script lang="ts">
 import Tab from "./Tab.vue";
-import { computed } from "vue";
+import { onMounted, ref } from "vue";
 export default {
   props: {
     selected: {
       type: String,
     },
   },
+
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref(null);
+    onMounted(() => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      console.log(navItems.value); //得到的是对象
+      console.log({ ...navItems.value });
+    });
+
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error("Tabs 子标签必须是 Tab");
       }
     });
-    // const current = computed(() => {
-    //   return defaults.filter((tag) => {
-    //     return tag.props.title === props.selected;
-    //   })[0];
-    // });
+
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
@@ -54,6 +71,8 @@ export default {
       defaults,
       titles,
       select,
+      navItems,
+      indicator,
     };
   },
 };
@@ -68,6 +87,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -78,6 +98,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
   &-content {
