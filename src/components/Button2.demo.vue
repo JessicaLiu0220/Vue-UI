e9608af248ca7452ace64115a0f6a9a04b248c32
Jessica <1330784336@qq.com>
2022-03-21 10:52:15 +0800

    改造button

---
+++ src/components/Button2.demo.vue
@@ -0,0 +1,29 @@
+<demo>支持size</demo>
+<template>
+  <div>
+    <div>
+      <Button size="big">大号</Button>
+      <Button>正常</Button>
+      <Button size="small">小号</Button>
+    </div>
+    <div>
+      <Button size="big" theme="link">大号</Button>
+      <Button theme="link">正常</Button>
+      <Button size="small" theme="link">小号</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大号</Button>
+      <Button theme="text">正常</Button>
+      <Button size="small" theme="text">小号</Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
