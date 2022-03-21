d41fcac34d3373b70966e79ffb189184bacdc964
Jessica <1330784336@qq.com>
2022-03-21 11:11:02 +0800

    改造Tabs

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav" ref="container">
@@ -7,7 +6,7 @@
         v-for="(t, index) in titles"
         :ref="
           (el) => {
-            if (el) navItems[index] = el;
+            if (t === selected) selectedItem = el;
           }
         "
         @click="select(t)"
@@ -19,55 +18,45 @@
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component
-        class="gulu-tabs-content-item"
-        v-for="(c, index) in defaults"
-        :class="{ selected: c.props.title === selected }"
-        :is="c"
-        :key="index"
-      />
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from "./Tab.vue";
-import { onMounted, ref, watchEffect } from "vue";
+import { computed, ref, watchEffect, onMounted } from "vue";
 export default {
   props: {
     selected: {
       type: String,
     },
   },
-
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
 
     onMounted(() => {
       watchEffect(() => {
-        const divs = navItems.value;
-        const result = divs.filter((div) =>
-          div.classList.contains("selected")
-        )[0];
-        const { width } = result.getBoundingClientRect();
+        const { width } = selectedItem.value.getBoundingClientRect();
         indicator.value.style.width = width + "px";
-
         const { left: left1 } = container.value.getBoundingClientRect();
-        const { left: left2 } = result.getBoundingClientRect();
+        const { left: left2 } = selectedItem.value.getBoundingClientRect();
         const left = left2 - left1;
         indicator.value.style.left = left + "px";
       });
     });
 
-    const defaults = context.slots.default(); //js获取插槽内容
+    const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error("Tabs 子标签必须是 Tab");
       }
     });
-
+    const current = computed(() => {
+      return defaults.find((tag) => tag.props.title === props.selected);
+    });
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
@@ -75,10 +64,11 @@ export default {
       context.emit("update:selected", title);
     };
     return {
+      current,
       defaults,
       titles,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
@@ -90,23 +80,28 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
+
 .gulu-tabs {
   &-nav {
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
     position: relative;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
+
     &-indicator {
       position: absolute;
       height: 3px;
@@ -117,14 +112,9 @@ $border-color: #d9d9d9;
       transition: all 250ms;
     }
   }
+
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
